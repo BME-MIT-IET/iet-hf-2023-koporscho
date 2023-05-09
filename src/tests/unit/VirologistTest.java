4d5b01e4afc6557504294df10562ad5872c0c3bb
szaboegon <szabo.egon2001@gmail.com>
2023-05-09 22:08:59 +0200

    Virologist unit tests finished

--- src/tests/unit/VirologistTest.java
+++ src/tests/unit/VirologistTest.java
@@ -1,9 +1,6 @@
 package tests.unit;
 
-import jdk.jshell.spi.ExecutionControl;
 import koporscho.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -70,7 +67,7 @@ class VirologistTest {
         assertThat(agentToCraft).isNotIn(virologist.GetAgentInventory());
     }
     @Test
-    void Should_apply_agent_to_target_and_remove_it_from_inventory(){
+    void Should_apply_agent_effect_to_target_and_remove_agent_from_inventory(){
         StatusEffect statusEffect = new StatusEffect();
         statusEffect.SetParalyzed(true);
         Agent agentToApply = new Agent(statusEffect,new Materials());
@@ -79,30 +76,108 @@ class VirologistTest {
 
         virologist.ApplyAgent(target, agentToApply);
 
+        ArrayList<StatusEffect> targetStatusEffect = target.GetStatusEffects();
+        var paralyzed = false;
+        for(StatusEffect se: targetStatusEffect){
+            if(se.GetParalyzed()){
+                paralyzed = true;
+            }
+        }
+        assertThat(paralyzed).isTrue();
         assertThat(agentToApply).isNotIn(virologist.GetAgentInventory());
     }
 
     @Test
-    void StealEquipment(){
-        throw new RuntimeException("Not implemented");
+    void Should_not_apply_agent_effect_if_target_is_immune(){
+        StatusEffect statusEffect = new StatusEffect();
+        statusEffect.SetParalyzed(true);
+        Agent agentToApply = new Agent(statusEffect,new Materials());
+        virologist.AddAgent(agentToApply);
+
+        Virologist target = new Virologist("Target");
+        StatusEffect immuneEffect = new StatusEffect();
+        immuneEffect.SetImmunity(1);
+        target.AddEffect(immuneEffect);
+
+        virologist.ApplyAgent(target, agentToApply);
+        ArrayList<StatusEffect> targetStatusEffect = target.GetStatusEffects();
+        var paralyzed = false;
+        for(StatusEffect se: targetStatusEffect){
+            if(se.GetParalyzed()){
+                paralyzed = true;
+            }
+        }
+        assertThat(paralyzed).isFalse();
     }
 
     @Test
-    void AddEquipment(){
-        throw new RuntimeException("Not implemented");
+    void Should_add_equipment_to_own_inventory_and_remove_it_from_target_when_stealing(){
+        Equipment equipmentToSteal = new Equipment(new StatusEffect(),0);
+        Virologist target = new Virologist("TargetVirologist");
+        target.AddEquipment(equipmentToSteal);
+        StatusEffect paralyzedEffect = new StatusEffect();
+        paralyzedEffect.SetParalyzed(true);
+        target.AddEffect(paralyzedEffect);
+        virologist.StealEquipment(target,equipmentToSteal);
+
+        assertThat(equipmentToSteal).isIn(virologist.GetEquipment());
+        assertThat(equipmentToSteal).isNotIn(target.GetEquipment());
     }
 
     @Test
-    void RemoveEquipment(){
-        throw new RuntimeException("Not implemented");
+    void Should_not_be_able_to_steal_if_target_is_not_paralyzed(){
+        Equipment equipmentToSteal = new Equipment(new StatusEffect(),0);
+        Virologist target = new Virologist("TargetVirologist");
+        target.AddEquipment(equipmentToSteal);
+        virologist.StealEquipment(target,equipmentToSteal);
+
+        assertThat(equipmentToSteal).isNotIn(virologist.GetEquipment());
+        assertThat(equipmentToSteal).isIn(target.GetEquipment());
     }
 
     @Test
-    void HandleSteal(){
-        throw new RuntimeException("Not implemented");
+    void Should_apply_equipment_effect_when_adding_equipment_to_inventory(){
+        StatusEffect equipmentEffect = new StatusEffect();
+        equipmentEffect.SetReflect(true);
+        Equipment equipment = new Equipment(equipmentEffect,0);
+
+        virologist.AddEquipment(equipment);
+
+        boolean reflect = false;
+        for(StatusEffect ef: virologist.GetStatusEffects()){
+            if(ef.GetReflect()){
+                reflect = true;
+            }
+        }
+        assertThat(reflect).isTrue();
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    void Should_remove_equipment_effect_when_removing_equipment_from_inventory(){
+        StatusEffect equipmentEffect = new StatusEffect();
+        equipmentEffect.SetReflect(true);
+        Equipment equipment = new Equipment(equipmentEffect,0);
+
+        virologist.AddEquipment(equipment);
+        virologist.RemoveEquipment(equipment);
+
+        boolean reflect = false;
+        for(StatusEffect ef: virologist.GetStatusEffects()){
+            if(ef.GetReflect()){
+                reflect = true;
+            }
+        }
+        assertThat(reflect).isFalse();
+    }
+    @Test
+    void Should_decrease_durability_of_axe_when_chopping(){
+        StatusEffect deadEffect = new StatusEffect();
+        deadEffect.SetDead(true);
+        Equipment axe = new Equipment(deadEffect,2);
+        virologist.AddEquipment(axe);
+        Virologist target = new Virologist("TargetVirologist");
+
+        virologist.Chop(target);
+        assertThat(axe.GetDurability()).isEqualTo(1);
     }
 }
